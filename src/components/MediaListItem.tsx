72db78b50f4fbdd1fe63f3284d9b6454fd963eab
eemiljka <eemiljka@metropolia.fi>
2024-03-18 23:05:52 +0200

    home-screen modifications and implementing likes. Likes unfinished, Home-screen unfinished

--- src/components/MediaListItem.tsx
+++ src/components/MediaListItem.tsx
@@ -4,6 +4,7 @@ import {Card, Icon, ListItem, Button} from '@rneui/base';
 import moment from 'moment';
 import {MediaItemWithOwner} from '../types/DBTypes';
 import {useUserContext} from '../hooks/ContextHooks';
+import colors from '../styles/colors';
 
 type Props = {
   item: MediaItemWithOwner;
@@ -13,54 +14,65 @@ type Props = {
 const MediaListItem = ({item, navigation}: Props) => {
   const {user} = useUserContext();
   return (
-    <Card containerStyle={{backgroundColor: '#144b29'}}>
-      <Text>
-        @{item.username} {moment(item.created_at).fromNow()}{' '}
-      </Text>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('Single Media', item);
+    <View style={{paddingBottom: 70}}>
+      <Card
+        containerStyle={{
+          backgroundColor: colors.lightgreen,
+          borderWidth: 0,
+          shadowColor: 'transparent',
         }}
       >
-        <Card.Image
-          style={{aspectRatio: 1, height: 300}}
-          source={{uri: 'http:' + item.thumbnail}}
-        />
-        <Card.Divider />
-        <Card.Title>{item.title}</Card.Title>
-        <Card.Divider />
-        <ListItem>
-          {user && user.user_id === item.user_id ? (
-            <>
-              <Button
+        <Text style={{color: colors.blue, fontSize: 20, paddingBottom: 10}}>
+          @{item.username}
+        </Text>
+        <TouchableOpacity
+          activeOpacity={1}
+          onPress={() => {
+            navigation.navigate('Single Media', item);
+          }}
+        >
+          <Card.Image
+            style={{aspectRatio: 1, height: 300}}
+            source={{uri: 'http:' + item.thumbnail}}
+          />
+
+          <Card.Title style={{color: colors.blue, paddingTop: 10}}>
+            {item.title}
+          </Card.Title>
+          <View style={{flexDirection: 'row', justifyContent: 'flex-end'}}>
+            {user && user.user_id === item.user_id ? (
+              <>
+                <Button
+                  onPress={() => {
+                    navigation.navigate('Modify', item);
+                  }}
+                >
+                  <Icon type="ionicon" name="create" color="white" />
+                </Button>
+                <Button
+                  color="error"
+                  onPress={() => {
+                    console.log('delete');
+                  }}
+                >
+                  {' '}
+                  <Icon type="ionicon" name="trash" color="white" />
+                </Button>
+              </>
+            ) : (
+              <TouchableOpacity
                 onPress={() => {
-                  navigation.navigate('Modify', item);
+                  navigation.navigate('Single Media', item);
                 }}
-              >
-                <Icon type="ionicon" name="create" color="white" />
-              </Button>
-              <Button
-                color="error"
-                onPress={() => {
-                  console.log('delete');
-                }}
-              >
-                {' '}
-                <Icon type="ionicon" name="trash" color="white" />
-              </Button>
-            </>
-          ) : (
-            <>
-              <Icon
-                type="ionicon"
-                name={item.media_type.includes('image') ? 'image' : 'film'}
-              />
-              <ListItem.Chevron color={'black'} />
-            </>
-          )}
-        </ListItem>
-      </TouchableOpacity>
-    </Card>
+              ></TouchableOpacity>
+            )}
+          </View>
+          <Text style={{paddingTop: 10, color: colors.blue}}>
+            {moment(item.created_at).fromNow()}{' '}
+          </Text>
+        </TouchableOpacity>
+      </Card>
+    </View>
   );
 };
 export default MediaListItem;
---
+++ src/styles/colors.ts
@@ -0,0 +1,7 @@
+export default {
+  darkgreen: '#0A1C13',
+  lightgreen: '#144B29',
+  mossgreen: '#86934F',
+  sage: '#D0D09B',
+  blue: '#ACCBED',
+};
