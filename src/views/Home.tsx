799bee925ef22b4c9ce98956f80d860c16b2e450
eemiljka <eemiljka@metropolia.fi>
2024-03-28 23:52:57 +0200

    SearchBar added to homepage

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,5 +1,7 @@
-import {FlatList} from 'react-native';
+import {FlatList, View} from 'react-native';
 import {NavigationProp, ParamListBase} from '@react-navigation/native';
+import {SearchBar} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import {useMedia} from '../hooks/apiHooks';
 import MediaListItem from '../components/MediaListItem';
 import colors from '../styles/colors';
@@ -9,6 +11,39 @@ const Home = ({navigation}: {navigation: NavigationProp<ParamListBase>}) => {
 
   return (
     <>
+      <View
+        style={{
+          backgroundColor: colors.lightgreen,
+        }}
+      >
+        <SearchBar
+          placeholder="Search for a recipe"
+          containerStyle={{
+            backgroundColor: colors.lightgreen,
+            borderBottomColor: 'transparent',
+            borderTopColor: 'transparent',
+          }}
+          inputContainerStyle={{backgroundColor: colors.mossgreen}}
+          inputStyle={{color: colors.darkgreen, fontSize: 20}}
+          placeholderTextColor={colors.darkgreen}
+          onBlur={undefined}
+          onChangeText={undefined}
+          onFocus={undefined}
+          value={''}
+          platform={'default'}
+          clearIcon={undefined}
+          searchIcon={<Icon name="search" color={colors.darkgreen} />}
+          loadingProps={undefined}
+          showLoading={false}
+          onClear={undefined}
+          onCancel={undefined}
+          lightTheme={false}
+          round={true}
+          cancelButtonTitle={''}
+          cancelButtonProps={undefined}
+          showCancel={false}
+        />
+      </View>
       <FlatList
         style={{backgroundColor: colors.lightgreen}}
         data={mediaArray}
