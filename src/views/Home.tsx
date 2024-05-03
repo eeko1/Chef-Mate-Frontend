e2c9ec100e4afda6d03ea05c343092dfa84be6ec
Eemi <eemi.korhonen@metropolia.fi>
2024-05-03 09:30:49 +0300

    searching works

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,4 +1,4 @@
-import {FlatList, View} from 'react-native';
+import {FlatList, View, Text} from 'react-native';
 import React, {useMemo, useState} from 'react';
 import {TextInput} from 'react-native-paper';
 import {useMedia} from '../hooks/apiHooks';
@@ -12,9 +12,24 @@ const Home = ({navigation}) => {
   const onChangeSearch = (query) => setSearchQuery(query);
 
   const filteredMedia = useMemo(() => {
-    return mediaArray.filter((item) =>
-      item.ingredients.toLowerCase().includes(searchQuery.toLowerCase()),
-    );
+    if (!searchQuery) return mediaArray;
+
+    const searchTerms = searchQuery
+      .toLowerCase()
+      .split(/\s+|,/)
+      .map((term) => term.trim())
+      .filter((term) => term);
+
+    return mediaArray.filter((item) => {
+      const ingredients = item.ingredients
+        .toLowerCase()
+        .split(/[,&]+| and /)
+        .map((ingredient) => ingredient.trim());
+
+      return searchTerms.every((term) =>
+        ingredients.some((ingredient) => ingredient.includes(term)),
+      );
+    });
   }, [searchQuery, mediaArray]);
 
   return (
@@ -33,14 +48,29 @@ const Home = ({navigation}) => {
           }}
         />
       </View>
-      <FlatList
-        style={{backgroundColor: colors.lightgreen}}
-        data={filteredMedia}
-        renderItem={({item}) => (
-          <MediaListItem navigation={navigation} item={item} />
-        )}
-        keyExtractor={(item) => item?.id?.toString()}
-      />
+      {filteredMedia.length > 0 ? (
+        <FlatList
+          style={{backgroundColor: colors.lightgreen}}
+          data={filteredMedia}
+          renderItem={({item}) => (
+            <MediaListItem navigation={navigation} item={item} />
+          )}
+          keyExtractor={(item) => item?.id?.toString()}
+        />
+      ) : (
+        <View
+          style={{
+            backgroundColor: colors.lightgreen,
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Text style={{color: 'black', fontSize: 20}}>
+            Could not find any recipes by those ingredients.
+          </Text>
+        </View>
+      )}
     </>
   );
 };
